28f715ef20f7200d3703443dddf89c4e71cfed0c
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2024-10-02 12:29:49 +0100

    Update Program.cs

--- C Sharp Recap/Program.cs
+++ C Sharp Recap/Program.cs
@@ -12,33 +12,21 @@
             float width = 0, height = 0;
 
             Console.WriteLine("Width:");
-            while (width == 0)
+            input = Console.ReadLine();
+            // if unable to parse - loop entering and trying to parse new input
+            while (!float.TryParse(input, out width))
             {
+                Console.WriteLine($"unable to parse '{input}'.");
                 input = Console.ReadLine();
-                try
-                {
-                    width = float.Parse(input);
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine($"unable to parse '{input}'");
-                }
             }
 
-
-
             Console.WriteLine("Height:");
-            while (height == 0)
+            input = Console.ReadLine();
+            // if unable to parse - loop entering and trying to parse new input
+            while (!float.TryParse(input, out height))
             {
+                Console.WriteLine($"unable to parse '{input}'.");
                 input = Console.ReadLine();
-                try
-                {
-                    height = float.Parse(input);
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine($"unable to parse '{input}'");
-                }
             }
 
             float area = width * height;
@@ -49,21 +37,64 @@
             // 3 & 4
             Console.WriteLine("Enter 10 integers");
             int[] numArray = new int[10];
+            int largest = 0;
             for (int i = 0; i < numArray.Length; i++)
             {
                 int temp;
                 string tempString;
-                Console.WriteLine("Integer " + i);
+                Console.WriteLine("Integer " + (i + 1) + ":");
                 tempString = Console.ReadLine();
-                try
+                while (!int.TryParse(tempString, out temp))
                 {
-                    temp = int.Parse(tempString);
+                    Console.WriteLine($"unable to parse '{tempString}'.");
+                    tempString = Console.ReadLine();
                 }
-                catch (FormatException)
+                numArray[i] = temp;
+            }
+
+            foreach (int i in numArray)
+            {
+                if (i > largest)
                 {
-                    Console.WriteLine($"unable to parse '{tempString}'");
+                    largest = i;
                 }
             }
+            Console.WriteLine("Largest Number Entered = " + largest);
+
+            for (int i = numArray.Length; i > 0; i--)
+            {
+                Console.WriteLine(numArray[i-1]);
+            }
+
+            Console.WriteLine("Result: " + Add());
+        }
+
+        // 5
+        static int Add()
+        {
+            string line;
+            int a = 0, b = 0;
+
+            Console.WriteLine("Number 1: ");
+            line = Console.ReadLine();
+
+            while (!int.TryParse(line, out a))
+            {
+                Console.WriteLine($"unable to parse '{line}'.");
+                line = Console.ReadLine();
+            }
+
+            Console.WriteLine("Number 2: ");
+            line = Console.ReadLine();
+
+            while (!int.TryParse(line, out b))
+            {
+                Console.WriteLine($"unable to parse '{line}'.");
+                line = Console.ReadLine();
+            }
+
+
+            return a + b; 
         }
     }
 }
