639ac397752b3e932b1f67ebc3884b76facef4c4
D761454 <144056074+D761454@users.noreply.github.com>
2024-10-02 15:17:56 +0100

    Update Program.cs

--- C Sharp Recap/Program.cs
+++ C Sharp Recap/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace C_Sharp_Recap
 {
@@ -9,7 +10,8 @@ namespace C_Sharp_Recap
             // 1
             Console.WriteLine("Hello, World!");
 
-            Task8();
+            Hangman.Hangman hangman = new Hangman.Hangman();
+            hangman.Update();
         }
 
         static void Task2()
@@ -164,7 +166,6 @@ namespace C_Sharp_Recap
             animalsArray[temp-1].Speak();
         } 
     }
-}
 
     internal class Animal
     {
@@ -178,10 +179,10 @@ namespace C_Sharp_Recap
 
     internal class Dog : Animal
     {
-    public Dog()
-    {
-        name = "Dog";
-    }
+        public Dog()
+        {
+            name = "Dog";
+        }
 
         public override void Speak()
         {
@@ -191,11 +192,11 @@ namespace C_Sharp_Recap
 
     internal class Cat : Animal
     {
-    public Cat()
-    {
-        name = "Cat";
-    }
-    public override void Speak()
+        public Cat()
+        {
+            name = "Cat";
+        }
+        public override void Speak()
         {
             Console.WriteLine("Meow.");
         }
@@ -203,12 +204,60 @@ namespace C_Sharp_Recap
 
     internal class Horse : Animal
     {
-    public Horse()
-    {
-        name = "Horse";
-    }
-    public override void Speak()
+        public Horse()
+        {
+            name = "Horse";
+        }
+        public override void Speak()
         {
             Console.WriteLine("Neigh.");
         }
     }
+}
+
+
+namespace Hangman
+{
+    class Hangman
+    {
+        //Member Variables
+        int m_Lives = 6;
+        List<char> m_IncorrectLetters;
+        List<char> m_CorrectLetters;
+        string[] m_Words = { };
+        string m_Word;
+        bool m_Won = false;
+        char m_PlayAgain = 'n';
+
+        public Hangman()
+        {
+            //init
+        }
+
+        public void Update()
+        {
+            char userInput;
+
+            //Check to see if the user has used a value 
+            while (m_IncorrectLetters.Contains((userInput = Char.ToLower(Console.ReadKey().KeyChar))))
+            {
+                Console.WriteLine(": has already been guessed");
+            }
+
+            //Clear the screen
+            Console.Clear();
+        }
+
+        //Draw Hangman
+        void ShowHangman()
+        {
+            Console.WriteLine("     |------+  ");
+            Console.WriteLine("     |      |  ");
+            Console.WriteLine("     |      " + (m_Lives < 6 ? "O" : ""));
+            Console.WriteLine("     |     " + (m_Lives < 4 ? "/" : "") + (m_Lives < 5 ? "|" : "") + (m_Lives < 3 ? @"\" : ""));
+            Console.WriteLine("     |     " + (m_Lives < 2 ? "/" : "") + " " + (m_Lives < 1 ? @"\" : ""));
+            Console.WriteLine("     |         ");
+            Console.WriteLine("===============");
+        }
+    }
+}
